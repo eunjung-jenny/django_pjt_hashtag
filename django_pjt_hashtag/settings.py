3b20f0765fdc91dfdeb9ee28b8c87c2948659e19
jenny.kr <jenny.it.kr@gmail.com>
2020-04-29 11:37:39 +0900

    Update style

--- django_pjt_hashtag/settings.py
+++ django_pjt_hashtag/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 AUTH_USER_MODEL = 'accounts.User'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
